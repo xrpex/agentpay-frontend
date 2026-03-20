e006e6e8111975159a3e8b1a5b4a97c1823faf8e
xrpex <anonhron3@gmail.com>
2026-03-20 22:59:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import MarketplacePage from "./pages/MarketplacePage.jsx";
 import ToolDetailPage from "./pages/ToolDetailPage.jsx";
@@ -8,7 +8,7 @@ import DocsPage from "./pages/DocsPage.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<MarketplacePage />} />
@@ -17,6 +17,6 @@ export default function App() {
           <Route path="docs" element={<DocsPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
