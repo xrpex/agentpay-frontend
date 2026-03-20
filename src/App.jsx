3750b1ce6f71a1703a5c68e4e1905e4cd37499d7
xrpex <anonhron3@gmail.com>
2026-03-20 15:36:31 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import MarketplacePage from "./pages/MarketplacePage.jsx";
+import ToolDetailPage from "./pages/ToolDetailPage.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import DocsPage from "./pages/DocsPage.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MarketplacePage />} />
+          <Route path="tool/:id" element={<ToolDetailPage />} />
+          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="docs" element={<DocsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
