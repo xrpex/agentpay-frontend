5d7d4ad92b9bdfa6d7e0298ebd0b3f1972592909
xrpex <anonhron3@gmail.com>
2026-03-23 16:16:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import MarketplacePage from "./pages/MarketplacePage.jsx";
 import ToolDetailPage from "./pages/ToolDetailPage.jsx";
